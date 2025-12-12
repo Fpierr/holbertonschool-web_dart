73b4660ce6f96de0b9b4d23f77ab8930dab79b3e
Fpierr <fanuelp9@gmail.com>
2025-12-12 19:48:50 +0100

    add property called id to the User class

---
+++ 0x02-dart-classes/5-json_to_user.dart
@@ -0,0 +1,40 @@
+class User {
+  int id;
+  String name;
+  int age;
+  double height;
+
+  // Constructor
+  User({
+    required this.id,
+    required this.name,
+    required this.age,
+    required this.height,
+  });
+
+  // Transformate object in Map
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'age': age,
+      'height': height,
+    };
+  }
+
+  // Constructor static fromJson
+  static User fromJson(Map<dynamic, dynamic> userJson) {
+    return User(
+      id: userJson['id'],
+      name: userJson['name'],
+      age: userJson['age'],
+      height: userJson['height'],
+    );
+  }
+
+  // Method toString to display
+  @override
+  String toString() {
+    return 'User(id : $id ,name: $name,  age: $age, height: $height)';
+  }
+}
